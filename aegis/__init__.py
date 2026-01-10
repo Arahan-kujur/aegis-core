945d89f77d92f7dc6da634abe7f1e0e2f1179324
Arahan-kujur <kujurarahan@gmail.com>
2026-01-10 13:47:02 +0530

    Release 1.0.0

--- aegis/__init__.py
+++ aegis/__init__.py
@@ -5,7 +5,7 @@ Aegis wraps LangChain-style agents and intercepts risky actions,
 requiring human approval before execution.
 """
 
-VERSION = "1.0.2"
+VERSION = "1.0.0"
 
 from aegis.wrapper import AegisWrapper
 
